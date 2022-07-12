4f20bbe4aa7864f830415d4f532cf62d2f4679f6
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-07-12 22:56:47 +0200

    finish project 3

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -1,4 +1,4 @@
-// local reviews data
+/* local reviews data */
 const reviews = [
   {
     id: 1,
@@ -36,4 +36,50 @@ const reviews = [
     text:
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
+  {
+    id: 5,
+    name: "khalid mesbah",
+    job: "frontend web developer",
+    img:
+      "person-1.jpeg",
+    text:
+      "Hi, I’m Khalid, a Junior Frontend Web Developer who is super lucky to find his passion early, I code mainly for fun that’s what I am enjoying, I have hundreds of pens and tens of projects exploring the field of Frontend Web Engineering, I am not a superman but I have superpowers like Patience, Consistency, Hard Work, Passion, Curiosity, Enthusiasm, Challenger Mindset, and Willingness to Continually Learn and Improve.",
+  },
 ];
+/* current review id => global variable */
+let currentReviewId = reviews.length - 1;
+/* buttons */
+const prevBtn = document.querySelector(`.prev-btn`);
+const nextBtn = document.querySelector(`.next-btn`);
+const randomBtn = document.querySelector(`.random-btn`);
+/* articles parts */
+const personImgEl = document.getElementById(`person-img`);
+const authorEl = document.getElementById(`author`);
+const jobEl = document.getElementById(`job`);
+const infoEl = document.getElementById(`info`);
+/* event listeners */
+nextBtn.addEventListener(`click`, () => {
+  currentReviewId < reviews.length - 1 ?
+    currentReviewId++ :
+    currentReviewId = 0;
+  updateReview();
+});
+prevBtn.addEventListener(`click`, () => {
+  currentReviewId > 0 ?
+    currentReviewId-- :
+    currentReviewId = reviews.length - 1;
+  updateReview();
+});
+randomBtn.addEventListener(`click`, () => {
+  currentReviewId = Math.floor(Math.random() * reviews.length);
+  updateReview();
+});
+const updateReview = () => {
+  const newReview = reviews[currentReviewId];
+  personImgEl.src = newReview.img;
+  authorEl.textContent = newReview.name;
+  jobEl.textContent = newReview.job;
+  infoEl.textContent = newReview.text;
+};
+/* load initial review */
+updateReview();
