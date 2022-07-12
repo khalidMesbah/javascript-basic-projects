54935497efd08539050460023dac7f0bdb272954
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-07-12 20:44:26 +0200

    finish project one

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [`green`, `red`, `rgba(133,122,200)`, `#f15025`,`magenta`,`cyan`,`lime`,`orange`,`black`,`yellow`,`purple`,`pink`,`crimson`];
+const btn = document.getElementById(`btn`);
+const color = document.querySelector(`.color`);
+const main = document.querySelector(`main`);
+btn.addEventListener(`click`, () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    color.textContent = randomColor;
+    main.style.backgroundColor = `${randomColor}`;
+});
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,14 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById(`btn`);
+const color = document.querySelector(`.color`);
+const main = document.querySelector(`main`);
+
+btn.addEventListener(`click`, () => {
+    let randomColor = `#`;
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * hex.length);
+        randomColor += hex[randomIndex];
+    }
+    color.textContent = randomColor;
+    main.style.backgroundColor = `${randomColor}`;
+});
