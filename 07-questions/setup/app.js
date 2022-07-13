874d308bcdaea303b12e11ae6650baf290a36112
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-07-13 11:11:55 +0200

    finish project 7

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
+// using selectors inside the element
+const questions = document.querySelectorAll(`.question`);
+questions.forEach(thisQuestion => {
+    const btn = thisQuestion.querySelector(`.question-btn`);
+    btn.addEventListener(`click`, () => {
+        questions.forEach(question => {
+            question === thisQuestion ?
+                question.classList.toggle(`show-text`) :
+                question.classList.remove(`show-text`);
+        });
+    });
+});
 // traversing the dom
+/* const btns = document.querySelectorAll(`.question-btn`);
+
+btns.forEach(btn => {
+    btn.addEventListener(`click`, e => {
+        document.querySelectorAll(`.question`).forEach(question => {
+            question.classList.remove(`show-text`);
+        });
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle(`show-text`);
+    });
+});
+ */
