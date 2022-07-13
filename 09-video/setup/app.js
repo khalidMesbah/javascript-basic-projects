ce26cfedc9a7e3756519029708780f40cdebb3a3
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-07-13 14:35:45 +0200

    finish project 9

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -1,3 +1,16 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const switchBtn = document.querySelector(`.switch-btn`);
+const video = document.querySelector(`.video-container`);
+const preloader = document.querySelector(`.preloader`)
+
+window.addEventListener(`load`,preloader.classList.add(`hide-preloader`))
+
+switchBtn.addEventListener(`click`, () => {
+    switchBtn.classList.toggle(`slide`);
+    switchBtn.classList.contains(`slide`) ?
+        video.pause() :
+        video.play();
+});
